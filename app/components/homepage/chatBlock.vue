b831a40aa020689db9f8443e1c70b719f750c8a8
iquldev <iquldev@proton.me>
2026-01-24 12:25:57 +0300

    feat: code refactor

--- app/components/homepage/chatBlock.vue
+++ app/components/homepage/chatBlock.vue
@@ -1,9 +1,6 @@
 <template>
   <motion.div
     layout
-    :transition="{
-      layout: { type: 'spring', stiffness: 300, damping: 30 },
-    }"
     class="flex flex-col h-full w-full items-center justify-center gap-8"
   >
     <h1 class="text-5xl font-bold text-(--ui-text-primary)">GChat</h1>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,8 +1,18 @@
+<template>
+  <LayoutGroup>
+    <motion.div layout class="h-screen p-6 flex flex-col md:flex-row gap-4">
+      <Sidebar class="shrink-0" />
+      <ChatBlock class="flex-1 min-w-0" />
+      <SettingsModal :is-settings-open="isSettingsOpen" />
+    </motion.div>
+  </LayoutGroup>
+</template>
+
 <script setup lang="ts">
 import Sidebar from "~/components/sidebar/sidebar.vue";
 import SettingsModal from "~/components/settings/settingsModal.vue";
 import ChatBlock from "~/components/homepage/chatBlock.vue";
-import { motion } from "motion-v";
+import { motion, LayoutGroup } from "motion-v";
 
 const isSettingsOpen = ref(false);
 
@@ -12,14 +22,6 @@ const toggleSettings = (value?: boolean) => {
 
 provide("toggleSettings", toggleSettings);
 </script>
-
-<template>
-  <div class="h-screen p-6 flex flex-col md:flex-row gap-4">
-    <Sidebar class="shrink-0" />
-    <ChatBlock class="flex-1 min-w-0" />
-    <SettingsModal :is-settings-open="isSettingsOpen" />
-  </div>
-</template>
 
 <style>
 * {
